084f2563f7881a2dcaaaf9035e21f0982393e4cb
Gabriel <gabrielpfernandez@gmail.com>
2025-03-04 23:00:15 +0100

    Counter program step.

--- temp-project/programs/temp-project/src/lib.rs
+++ temp-project/programs/temp-project/src/lib.rs
@@ -6,11 +6,26 @@ declare_id!("AZ6jm7itX4h4w7hE8MkyK59QtE1yR9PUCrKEEeFu6PGG");
 pub mod temp_project {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+}
+
+const DISCRIMNATOR: usize = 8;
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init,
+        payer = user,
+        space = DISCRIMNATOR + Counter::INIT_SPACE
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
